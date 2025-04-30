604419d369c7d79fce5bb85d5efedf9fc579f367
NielsvN <nvn@upcmail.nl>
2025-04-30 21:29:27 +0200

    Niks belangrijks, ff testen of dit werkt met pycharm

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AE4204-KBE-TL-NvN-2025
 Knowledge based engineering project repository of team Niels van Nieuwland and Ties Leniger
+
+ff testen hoor
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from OCC.wrapper.StepVisual import StepVisual_HArray1OfSurfaceStyleElementSelect_get_type_name
 from parapy.geom import Box
 from parapy.gui import display
 
 obj = Box(1, 2, 3)
 display(obj)
+
+nog meer testen
