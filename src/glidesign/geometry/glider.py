6798f974ae8eb909c54241f613930806e0a3bfd3
tleniger <95964164+TiesLeniger@users.noreply.github.com>
2025-05-01 15:11:49 +0200

    start on glider.py (top class)

---
+++ src/glidesign/geometry/glider.py
@@ -0,0 +1,12 @@
+# Python native imports
+
+# Python third party imports
+import numpy as np
+
+# ParaPy imports
+from parapy.geom import GeomBase, translate, rotate, MirroredShape
+from parapy.core import Input, Attribute, Part
+
+class Glider(GeomBase):
+
+    pass
