4786353b0e58ae7578d4e2c6621aec558f6f77bc
tleniger <95964164+TiesLeniger@users.noreply.github.com>
2025-05-16 14:14:07 +0200

    validator that checks input and kbeutils for airfoil_name.dat

---
+++ src/glidesign/core/__init__.py
@@ -0,0 +1,1 @@
+from validator import airfoil_found
--- src/glidesign/core/validator.py
+++ src/glidesign/core/validator.py
@@ -9,6 +9,7 @@ import os
 from kbeutils.data import airfoils
 from parapy.core.validate import AdaptedValidator
 
-kbeutils_airfoil_found = AdaptedValidator(lambda name: os.path.isfile(os.path.join(airfoils.__path__[0], name + ".dat")))
-
-input_airfoil_found = AdaptedValidator(lambda name: os.path.isfile(os.path.join(os.getcwd(), "input", "airfoils", name + ".dat")))
+airfoil_found = AdaptedValidator(lambda name: (
+        os.path.isfile(os.path.join(os.getcwd(), "input", "airfoils", name + ".dat")) or
+        os.path.isfile(os.path.join(airfoils.__path__[0], name + ".dat"))
+    ))
