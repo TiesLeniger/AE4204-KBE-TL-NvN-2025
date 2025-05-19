1bd5c4efa690b86d5f134a8c351b2b09ae6d7161
NielsvN <nvn@upcmail.nl>
2025-05-19 18:33:54 +0200

    We hebben soortvan een fuselage, hij ziet er alleen wel wack uit...

--- src/glidesign/geometry/fuselage.py
+++ src/glidesign/geometry/fuselage.py
@@ -4,7 +4,8 @@
 import numpy as np
 
 # ParaPy imports
-from parapy.geom import GeomBase, Circle, BSplineCurve, Point, Vector, translate, rotate, MirroredShape
+from parapy.geom import GeomBase, Circle, BSplineCurve, Point, Vector, translate, rotate, MirroredShape, LoftedSurface, \
+    Position, LoftedShell
 from parapy.core import Input, Attribute, Part, child
 from parapy.core.validate import OneOf, Range, GreaterThan
 
@@ -19,7 +20,7 @@ class GliderFuselage(GeomBase):
     length: float = Input()
     max_diameter: float = Input()
 
-    relative_section_diameter: list[float] = Input([10, 50, 80, 95, 100, 75, 45, 25, 15, 10], validator = Range(1, 100)) #Percentage of maximum fuselage diameter
+    relative_section_diameter: list[float] = Input([5, 40, 60, 80, 90, 100, 100, 100, 100, 90, 80, 70, 60, 50, 40, 35, 33, 29, 28, 27, 26, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25]) #Percentage of maximum fuselage diameter
 
     @Attribute
     def section_diameter(self) -> list[float]:
@@ -32,45 +33,26 @@ class GliderFuselage(GeomBase):
     @Attribute
     def spine_points(self):
         return [
-            Point(0.2432, 0, 0.0021),
-            Point(0.5012, 0, 0.0144),
-            Point(0.7532, 0, 0.0144),
-            Point(1.0053, 0, 0.0433),
-            Point(1.2485, 0, 0.0680),
-            Point(1.4739, 0, 0.0948),
-            Point(1.6904, 0, 0.1196),
-            Point(1.8831, 0, 0.1402),
-            Point(2.1263, 0, 0.1629),
-            Point(2.3280, 0, 0.1649),
-            Point(2.5177, 0, 0.1629),
-            Point(2.7402, 0, 0.1608),
-            Point(2.9033, 0, 0.1526),
-            Point(3.1109, 0, 0.1402),
-            Point(3.3184, 0, 0.1196),
-            Point(3.5468, 0, 0.1052),
-            Point(3.7870, 0, 0.1010),
-            Point(4.0391, 0, 0.0887),
-            Point(4.2823, 0, 0.0845),
-            Point(4.5432, 0, 0.0804),
-            Point(4.8072, 0, 0.0722),
-            Point(5.1037, 0, 0.0680),
-            Point(5.3884, 0, 0.0701),
-            Point(5.7205, 0, 0.0639),
-            Point(6.0112, 0, 0.0619),
-            Point(6.2959, 0, 0.0639) #Points retreived from approximately the AS33 fuselage
+            Point(0.0, 0, 0.0),               # nose
+            Point(1.5, 0, 0.2),               # cockpit rise
+            Point(2.3, 0, 0.4),
+            Point(3.0, 0, 0.6),               # max diameter
+            Point(4.5, 0, 0.6),               # beginning taper
+            Point(6.0, 0, 0.6),               # tail boom
+            Point(self.length, 0, 0.6)                # tail tip
         ]
 
-    @Attribute
+    @Part
     def fuselage_spine(self):
-        return BSplineCurve(points=self.spine_points,
-                                 color='black'
-                                )
+        return BSplineCurve(self.spine_points,
+                            color='black'
+                            )
 
     @Attribute
     def x_equidistant_points_bspline(self):
         num_samples = 1000
         t_samples = np.linspace(0, 1, num_samples)
-        points = [self.bspline_curve.position_at(t).point for t in t_samples]
+        points = [self.fuselage_spine.point_at_parameter(t) for t in t_samples]
         x_vals = [pt.x for pt in points]
 
         x_start = 0
@@ -79,15 +61,31 @@ class GliderFuselage(GeomBase):
 
         t_targets = np.interp(x_targets, x_vals, t_samples)
 
-        return [self.bspline_curve.position_at(t) for t in t_targets]
+        return [self.fuselage_spine.point_at_parameter(t) for t in t_targets]
 
     @Part
     def fuselage_profiles(self):
-        return Circle(quantify=len(self.sections),
-                      color = 'white',
+        return Circle(quantify=len(self.relative_section_diameter),
+                      color = 'black',
                       radius = self.section_diameter[child.index]/2, #Convert diameter into radius
-                      position = translate(self.position.rotate90('y'),
-                                           self.x_equidistant_points_bspline[child.index])
+                      position=translate(self.position,
+                                         'x', self.x_equidistant_points_bspline[child.index].x,
+                                         'y', self.x_equidistant_points_bspline[child.index].y,
+                                         'z', self.x_equidistant_points_bspline[child.index].z
+                                         ).rotate('y', 90, deg=True)
+                      # Apply per-profile rotation
                       )
 
+    @Part
+    def fuselage_lofted(self):
+        return LoftedShell(profiles = self.fuselage_profiles,
+                           ruled=True,
+                           color = self.color,
+                           hidden = not(__name__ == '__main__')
+        )
 
+if __name__ == '__main__':
+    from parapy.gui import display
+    fus = GliderFuselage(name="fuselage", max_diameter= 0.75, length= 6.5)
+    print(fus.x_equidistant_points_bspline)
+    display(fus)
