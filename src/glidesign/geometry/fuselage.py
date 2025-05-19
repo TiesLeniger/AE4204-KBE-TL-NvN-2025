6a1e9ed45c7ee83f04eeb36ead969df0d00b6f66
NielsvN <nvn@upcmail.nl>
2025-05-19 13:36:27 +0200

    Postioned curves along the fuselage spine.

--- src/glidesign/geometry/fuselage.py
+++ src/glidesign/geometry/fuselage.py
@@ -4,13 +4,12 @@
 import numpy as np
 
 # ParaPy imports
-from parapy.geom import GeomBase, Circle, InterpolatedCurve, Point, Vector, translate, rotate, MirroredShape
+from parapy.geom import GeomBase, Circle, BSplineCurve, Point, Vector, translate, rotate, MirroredShape
 from parapy.core import Input, Attribute, Part, child
 from parapy.core.validate import OneOf, Range, GreaterThan
 
 # Custom imports
-from ..core import airfoil_found
-from .airfoil import Airfoil
+#from .airfoil import Airfoil
 
 class GliderFuselage(GeomBase):
 
@@ -58,23 +57,37 @@ class GliderFuselage(GeomBase):
             Point(5.3884, 0, 0.0701),
             Point(5.7205, 0, 0.0639),
             Point(6.0112, 0, 0.0619),
-            Point(6.2959, 0, 0.0639)
+            Point(6.2959, 0, 0.0639) #Points retreived from approximately the AS33 fuselage
         ]
 
     @Attribute
     def fuselage_spine(self):
-        return InterpolatedCurve(points=self.spine_points,
-                                 initial_tangent=Vector(1, 0, 0),
-                                 final_tangent=Vector(1, 0, 0),
+        return BSplineCurve(points=self.spine_points,
                                  color='black'
                                 )
+
+    @Attribute
+    def x_equidistant_points_bspline(self):
+        num_samples = 1000
+        t_samples = np.linspace(0, 1, num_samples)
+        points = [self.bspline_curve.position_at(t).point for t in t_samples]
+        x_vals = [pt.x for pt in points]
+
+        x_start = 0
+        x_end = self.length
+        x_targets = np.linspace(x_start, x_end, len(self.relative_section_diameter))
+
+        t_targets = np.interp(x_targets, x_vals, t_samples)
+
+        return [self.bspline_curve.position_at(t) for t in t_targets]
 
     @Part
     def fuselage_profiles(self):
         return Circle(quantify=len(self.sections),
                       color = 'white',
-                      radius = self.section_diameter[child.index]/2 #Convert diameter into radius
-                      position =
+                      radius = self.section_diameter[child.index]/2, #Convert diameter into radius
+                      position = translate(self.position.rotate90('y'),
+                                           self.x_equidistant_points_bspline[child.index])
                       )
 
 
