d8ea6c189e3e7df936949ff867fbcb7bda102a2e
NielsvN <nvn@upcmail.nl>
2025-05-20 19:30:32 +0200

    Nog niet helemaal af, krijg de axis of rotation rond de fuselage_outerline niet goed. En er zitten 2 discontinuities op xi en xm

--- src/glidesign/geometry/fuselage2.py
+++ src/glidesign/geometry/fuselage2.py
@@ -1,6 +1,20 @@
-from parapy.core import *
-from parapy.geom import *
+# Python native imports
 import math
+
+
+# Python third party imports
+
+
+# ParaPy imports
+from parapy.geom import GeomBase, Circle, BSplineCurve, Point, PointCloud, Vector, translate, rotate, MirroredShape, LoftedSurface, \
+    Position, LoftedShell, FittedCurve, Revolution, RevolvedSurface
+from parapy.core import Input, Attribute, Part, child
+from parapy.core.validate import OneOf, Range, GreaterThan, Positive
+from wx.lib.floatcanvas.FCObjects import PointSet
+
+
+# Custom imports
+#from .airfoil import Airfoil
 
 #--------------------------------------
 #Version under construction:
@@ -15,49 +29,51 @@ class GliderFuselage(GeomBase):
     color: str = Input('white')
 
     # Design parameters
-    xm = Input(0.4)     # X_m / L
-    k1 = Input(0.2)     # curvature at X_m
-    rn = Input(0.05)    # radius of curvature at nose (non-dimensional)
-    ri = Input(0.6)     # profile radius at X_i
-    si = Input(0.5)     # profile slope at X_i
-    xi = Input(0.8)     # location of inflection point (non-dimensional)
-    phi = Input(math.radians(10))  # trailing-edge angle in radians
-    L = Input(10.0)     # fuselage length
-    D = Input(1.0)      # max diameter
+    xm = Input(0.35, validator = Range(0, 0.6))                     # X_m / L
+    k1 = Input(0.17, validator = Positive)                                      # curvature at X_m
+    rn = Input(0.349, validator = Positive)                                     # radius of curvature at nose (non-dimensional)
+    ri = Input(0.469, validator = Range(0, 1))                       # profile radius at X_i
+    si = Input(1.666, validator = Positive)                                     # profile slope at X_i
+    xi = Input(0.855, validator = Range(0.3, 1))                     # location of inflection point (non-dimensional)
+    phi = Input(6.32, validator = Range(6.3, 6.4))                    # trailing-edge angle in radians
+    L = Input(7)                                                               # fuselage length in meter
+    D = Input(1.0)                                                              # max diameter
 
-    fr = 50 #derived(lambda self: self.L / self.D)  # f_r
+    #TODO: make this fr (L/D) iterative with the analysis:
+    fr = 10 #derived(lambda self: self.L / self.D)  #Lift-to-Drag ratio (from analysis)
 
     # --------- Forebody ---------
-    @Attribute
-    def F1(self):
-        return -2 * (x - 1) ** 3
+    def f1(self, x):
+        return -2 * x * (x - 1) ** 3
 
-    @Attribute
-    def F2(self):
-        return -x**2 * (x - 1) ** 2
+    def f2(self, x):
+        return -x ** 2 * (x - 1) ** 2
 
-    @Attribute
-    def G(self):
-        return x**2 * (3 * x**2 - 8 * x + 6)
+    def g(self, x):
+        return x ** 2 * (3 * x ** 2 - 8 * x + 6)
 
-
-    def r_forebody(self):
-        x_hat = x / (self.xm * self.L)
-        val = (1 / (2 * self.fr)) * (self.rn * self.F1(x_hat) + self.k1 * self.F2(x_hat) + self.G(x_hat))
-        return math.sqrt(val)
+    def r_forebody(self, x):
+        x_hat = x / (self.xm * self.L) #Location along forebody (relative)
+        value_fore = (1 / (2 * self.fr)) * (self.rn * self.f1(x_hat) + self.k1 * self.f2(x_hat) + self.g(x_hat))
+        return math.sqrt(value_fore)
 
     # --------- Midbody ---------
-    def F1_mid(self, x): return -0.5 * x**3 * (x - 1)**2
-    def F2_mid(self, x): return -x**3 * (3 * x**2 - 8 * x + 6)
-    def G_mid(self, x): return x**3 * (6 * x**2 - 15 * x + 10)
+    def f1_mid(self, x):
+        return -0.5 * x ** 3 * (x - 1) ** 2
+
+    def f2_mid(self, x):
+        return x - x ** 3 * (3 * x ** 2 - 8 * x + 6)
+
+    def g_mid(self, x):
+        return x ** 3 * (6 * x ** 2 - 15 * x + 10)
 
     def r_midbody(self, x):
-        x_hat = (self.xi * self.L - x) / ((self.xi - self.xm) * self.L)
+        x_hat = (self.xi * self.L - x) / ((self.xi - self.xm) * self.L) #Location along midbody (1 if at xm, 0 if at xi)
         k1m = ((self.xi / self.xm) - 1) ** 2 * self.k1 / (1 - self.ri)
-        val = (1 / (2 * self.fr)) * (
-            self.ri + (1 - self.ri) * (k1m * self.F1_mid(x_hat) + self.si * self.F2_mid(x_hat) + self.G_mid(x_hat))
+        value_mid = (1 / (2 * self.fr)) * (
+                self.ri + (1 - self.ri) * (k1m * self.f1_mid(x_hat) + self.si * self.f2_mid(x_hat) + self.g_mid(x_hat))
         )
-        return val
+        return value_mid
 
     # --------- Afterbody ---------
     @Attribute
@@ -65,38 +81,47 @@ class GliderFuselage(GeomBase):
         return ((1 - self.ri) * (1 - self.xi) * self.si) / ((self.xi - self.xm) * self.ri)
 
     @Attribute
-    def s_iL(self):
-        return 2 * self.fr * (self.xi - self.xm) * math.tan(self.phi) / (1 - self.ri)
+    def s_il(self):
+        return (2 * self.fr * (self.xi - self.xm) * math.tan(self.phi)) / (1 - self.ri)
 
     def r_afterbody(self, x):
         x_hat = x / self.L
-        term1 = self.s_iL * x_hat * (1 - x_hat ** 3)
+        term1 = self.s_il * x_hat * (1 - x_hat ** 3)
         term2 = -self.s_ia * x_hat ** 2 * (2 * x_hat - 3) * (x_hat - 1)
         term3 = x_hat ** 2 * (3 * x_hat ** 2 - 8 * x_hat + 6)
         return term1 + term2 + term3
 
     # --------- Full Profile Points ---------
-    @Part
+    @Attribute
     def profile_points(self):
         def get_r(x):
             if 0 <= x <= self.xm * self.L:
                 return self.r_forebody(x)
             elif self.xm * self.L < x <= self.xi * self.L:
                 return self.r_midbody(x)
-            else:
+            elif self.xi * self.L < x <= self.L:
                 return self.r_afterbody(x)
 
-        num_points = 100
+        num_points = int(self.L * 100) #100 points per meter (1 each cm)
         dx = self.L / (num_points - 1)
-        pts = [Point(x=x, y=0, z=get_r(x) * self.D / 2) for x in [i * dx for i in range(num_points)]]
+        pts = [Point(x=x, y=0, z=(get_r(x) * self.D / 2)) for x in [i * dx for i in range(num_points)]]
         return pts
 
     @Part
+    def point_line(self):
+        return PointCloud(self.profile_points)
+
+    @Part
+    def fuselage_outerline(self):
+        return FittedCurve(points=self.profile_points,
+                           color='black'
+        )
+
+    @Part
     def fuselage_surface(self):
-        return RevolvedSurface(
-            profile=Polyline(points=self.profile_points),
-            axis=Line(Point(0, 0, 0), Point(self.L, 0, 0)),
-            angle=360
+        return RevolvedSurface(basis_curve=self.fuselage_outerline,
+
+                               color=self.color
         )
 
 
@@ -105,4 +130,5 @@ class GliderFuselage(GeomBase):
 if __name__ == '__main__':
     from parapy.gui import display
     fus = GliderFuselage(name="fuselage")
+    print(fus.profile_points)
     display(fus)
