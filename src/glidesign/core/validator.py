4fd662ba1ae9fce1eb4b9f467c7ba2b8e4e3680d
tleniger <95964164+TiesLeniger@users.noreply.github.com>
2025-05-21 15:24:35 +0200

    added custom validator for checking lengths of attribute lists

--- src/glidesign/core/validator.py
+++ src/glidesign/core/validator.py
@@ -13,3 +13,12 @@ airfoil_found = AdaptedValidator(lambda name: (
         os.path.isfile(os.path.join(os.getcwd(), "input", "airfoils", name + ".dat")) or
         os.path.isfile(os.path.join(airfoils.__path__[0], name + ".dat"))
     ))
+
+def validate_equal_length_lists(object, attribute_names: list[str]):
+    list_attributes = [getattr(object, name) for name in attribute_names]
+    lengths = [len(lst) for lst in list_attributes]
+    lengths_unique = set(lengths)
+    if len(lengths_unique) > 1:
+        raise ValueError(f"All section-related input lists must have the same length. "
+                         f"Got lengths: {lengths} for attributes {attribute_names}")
+    return True
