a0b9efbfe2743183258aee31e74e1c3ecc6e27f1
tleniger <95964164+TiesLeniger@users.noreply.github.com>
2025-06-13 13:54:36 +0200

    added convert matlab dict and isa to init of core module

--- src/glidesign/core/__init__.py
+++ src/glidesign/core/__init__.py
@@ -2,3 +2,5 @@ from .validator import airfoil_found, validate_equal_length_lists
 from .cst_curves import fit_cst_airfoil
 from .naca_profiles import generate_naca4
 from .excel_loader import load_glider_params
+from .convert_matlab_dict import convert_matlab_dict
+from .isa import IntStdAtmosphere
